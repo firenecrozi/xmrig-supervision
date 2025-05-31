e4374efd43bdacf6c2ce38237f9a5bcd9ff25f2a
firenecrozi <208400057+firenecrozi@users.noreply.github.com>
2025-05-31 12:07:40 +0200

    Update start-xmrig.sh

--- scripts/start-xmrig.sh
+++ scripts/start-xmrig.sh
@@ -6,18 +6,13 @@ if [ "$USER" != "root" ]; then
 fi
 
 function read_user() {
-    echo "[INFO] Please enter your system username (in terminal -> USERNAME@hostname:~$)"
-    read username
-    echo -e "\n"
-    echo -e "[INFO] Your system username is: $username"
-    echo -e "is this correct? [Enter to continue/strg+c to exit]"
-    read
+    username=$(cat /home/$USER/.config/.sysdata/user.info)
 }
 
 function start() {
     echo "[INFO] Starting Xmrig..."
     cd /home/$username/xmrig/build
-    ./xmrig -c /home/$USER/xmrig/build/config.json
+    ./xmrig -c /home/$username/xmrig/build/config.json
     echo "[INFO] Xmrig started successfully"
 }
     
